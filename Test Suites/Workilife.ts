a0d85efc77650658a0b55aaefd343f574b2cd46d
Suraj Kumar <Suraj Kumar@PUN-IT000636>
2019-09-17 23:34:55 +0530

    Suite addition

---
+++ Test Suites/Workilife.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Workilife</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ed7a5067-6343-4a8c-a8a7-bb434049b915</testSuiteGuid>
+</TestSuiteEntity>
